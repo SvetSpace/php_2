0b0f1547411d8c4720fa35c597d2f80d15cd7c27
SvetSpace <svetikt2001@gmail.com>
2021-11-16 12:11:05 +0800

    Controllers

---
+++ controllers/TwigBaseController.php
@@ -0,0 +1,31 @@
+<?php
+require_once "BaseController.php"; // обязательно импортим BaseController
+
+class TwigBaseController extends BaseController {
+    public $title = ""; // название страницы
+    public $template = ""; // шаблон страницы
+    //protected \Twig\Environment $twig; // ссылка на экземпляр twig, для рендернига
+    
+    // пишем конструктор, 
+    // передаем в него один параметр
+    // собственно ссылка на экземпляр twig (Dependency Injection)
+    public function __construct($twig)
+    {
+        $this->twig = $twig; // пробрасываем его внутрь
+    }
+    
+    // переопределяем функцию контекста
+    public function getContext() : array
+    {
+        $context = parent::getContext(); // вызываем родительский метод
+        $context['title'] = $this->title; // добавляем title в контекст
+
+        return $context;
+    }
+    
+    // функция гет, рендерит результат используя $template в качестве шаблона
+    // и вызывает функцию getContext для формирования словаря контекста
+    public function get() {
+        echo $this->twig->render($this->template, $this->getContext());
+    }
+}
