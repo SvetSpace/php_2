c4d083619c151202a5e3b499166b43ab46ddd024
SvetSpace <svetikt2001@gmail.com>
2021-11-16 12:37:02 +0800

    vamp

---
+++ controllers/VampController.php
@@ -0,0 +1,21 @@
+<?php
+require_once "TwigBaseController.php"; // импортим TwigBaseController
+
+class VampController extends TwigBaseController {
+    public $template = "vamp__object.twig";
+    public $title = "Вампир";
+
+    public function getContext(): array
+    {
+        $context = parent::getContext();
+
+        $context['is_image'] = $context['url'] == "/vamp/image";
+        $context['is_info'] = $context['url'] == "/vamp/info";
+        $context['is_poem'] = $context['url'] == "/vamp/poem";
+        $context['url_image'] = "/vamp/image";
+        $context['url_info'] = "/vamp/info";
+        $context['url_poem'] = "/vamp/poem";
+
+        return $context;
+    }
+}
